e148f957c15050d67fd9c8e7826e99ee03dc6c55
yush <389636851@qq.com>
2017-09-22 09:25:31 +0800

    Vue电商PC

---
+++ src/components/header-nav.vue
@@ -0,0 +1,70 @@
+<template>
+	<div id="header">
+		<div class="nav-global">
+			<div class="container">
+				<h1 class="nav-logo">
+					<a href="javascript:;"></a>
+				</h1>
+				<ul class="nav-aside">
+					<li class="nav-user">
+					  <router-link to="/account">个人中心</router-link>
+						<!--active-->
+						<div class="nav-user-wrapper">
+							<div class="nav-user-list">
+								<dl class="nav-user-avatar">
+									<dd><span class="ng-scope"></span></dd>
+									<dt class="ng-binding">+86 138****9453</dt>
+								</dl>
+								<ul>
+									<li class="order"><router-link to="/account" exact>我的订单</router-link></li>
+									<!--<li class="support"><a href="javascript:;">售后服务</a></li>
+									<li class="coupon"><a href="javascript:;">我的优惠</a></li>
+									<li class="information"><a href="javascript:;">账户资料</a></li>-->
+									<li class="address"><router-link to="/address">收货地址</router-link></li>
+								<!--	<li class="logout"><a href="javascript:;">退出</a></li>-->
+								</ul>
+							</div>
+						</div>
+					</li>
+					<!--active-->
+					<car-panel class="nav-cart"></car-panel>
+				</ul>
+				<ul class="nav-list">
+					<li><a href="javascript:;">在线商城</a></li>
+					<li><a href="javascript:;">坚果 Pro</a></li>
+					<li><a href="javascript:;">Smartisan M1 / M1L</a></li>
+					<li><a href="javascript:;">Smartisan OS</a></li>
+					<li><a href="javascript:;">欢喜云</a></li>
+					<li><a href="javascript:;">应用下载</a></li>
+					<li><a href="javascript:;">官方论坛</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="nav-sub">
+			<div class="nav-sub-wrapper">
+				<div class="container">
+					<ul class="nav-list">
+						<li><a href="javascript:;">首页</a></li>
+						<li><a href="javascript:;">手机</a></li>
+						<li><a href="javascript:;">“足迹系列”手感膜</a></li>
+						<router-link to="/" exact tag="li" activeClass="active">
+						    <a>官方配件</a>
+						</router-link>
+						<li><a href="javascript:;">周边产品</a></li>
+						<li><a href="javascript:;">第三方配件</a></li>
+						<li><a href="javascript:;">全部商品</a></li>
+						<li><a href="javascript:;">服务</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import CarPanel from '@/components/car-panel'
+export default {
+  components: {
+    CarPanel
+  }
+}
+</script>
---
+++ src/views/shop.vue
@@ -0,0 +1,101 @@
+<template>
+	<div id="main">
+		<div class="sku-box store-content">
+			<div class="sort-option">
+				<ul class="line clear">
+					<li><a href="javascript:;" class="active">综合排序</a></li>
+					<li><a href="javascript:;" class="">销量排序</a></li>
+					<li><a href="javascript:;" class="">价格低到高</a></li>
+					<li><a href="javascript:;" class="">价格高到低</a></li>
+				</ul>
+			</div>
+			<div class="gray-box">
+				<div class="item-box">
+					<shop-item key="index" v-for="item,index in goodsList" :item="item"></shop-item>
+				</div>
+			</div>
+		</div>
+    <prompt v-if="maxCount"></prompt>
+	</div>
+</template>
+
+
+<script>
+import goodData from '@/lib/newGoodsData'
+import shopItem from '@/components/shop-item'
+import prompt from '@/components/prompt'
+export default {
+  data () {
+    return {
+      goodsList: goodData
+    }
+  },
+  components: {
+    shopItem,
+    prompt
+  },
+  computed: {
+    maxCount () {
+      return this.$store.state.maxOff
+    }
+  }
+}
+</script>
+
+<style>
+.sku-box{
+    position: relative;
+}
+.sort-option{
+    border-top: 1px solid #D8D8D8;
+    color: #999;
+}
+.sort-option ul{
+    height: 60px;
+    line-height: 60px;
+}
+.sort-option li{
+    position: relative;
+    float: left;
+    padding-left: 42px;
+}
+.sort-option li:first-child{
+	padding-left: 9px;
+}
+.sort-option li:before{
+	content: ' ';
+    display: block;
+    position: absolute;
+    left: 20px;
+    top: 50%;
+    width: 2px;
+    height: 2px;
+    margin-top: -1px;
+    background: #C7C7C7;
+}
+.sort-option li:first-child:before{
+	display: none;
+}
+.sort-option a{
+	display: block;
+    font-size: 12px;
+    color: #999;
+}
+.sort-option a.active, .sort-option a:hover{
+    color: #5683EA;
+}
+.gray-box{
+	overflow: hidden;
+    background: #fff;
+    border-radius: 8px;
+    border: 1px solid #dcdcdc;
+    border-color: rgba(0,0,0,.14);
+    box-shadow: 0 3px 8px -6px rgba(0,0,0,.1);
+}
+.sku-box .item-box{
+	clear: both;
+    overflow: hidden;
+    margin: 0 -1px -1px -1px;
+}
+
+</style>
